10d7ba6bdb5136d6e16db06fa14c7e9e897f68f3
@MizaelGalvez <mizaelgalvez@gmail.com>
2017-06-20 20:10:14 -0700

    REPL
    explicacion sencilla antes de iniciar.

---
+++ introREPL.js
@@ -0,0 +1,11 @@
+// REPL son las iniciales de READ, EVALUATED, PRINT, LOOP, es una metodologia para evaluar nuestro codigo
+//
+
+
+var saludo = "hola REPL";
+var x = 25;
+
+
+
+// para cargar nuetro archivo, entramos en consola, e ingresamos al Bash de NODE, despues simplemente utilizamos lo siguiente
+// .load ruta_al_archivo/nombre_del_archivo.js y listo quedara cargado nuestro documento JS para verificar los vaklores
